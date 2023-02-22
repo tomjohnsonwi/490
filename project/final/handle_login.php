9550a1597212ecd7b1bec4298f5576fc82e221c2
tomjohnsonwi <92746305+tomjohnsonwi@users.noreply.github.com>
2023-02-21 21:41:07 -0600

    admin login

--- project/final/handle_login.php
+++ project/final/handle_login.php
@@ -8,22 +8,31 @@ $password = mysqli_real_escape_string($dbc, trim($_POST['password']));
 
 // query
 $query = "SELECT * from users WHERE username = '$username' AND pw = SHA2('$password', 224)";
+$guestquery = "SELECT * from users WHERE username = 'guest' AND pw = 'guest'";
   
 // run query
 $result = mysqli_query($dbc, $query);
+$guestresult = mysqli_query($dbc, $guestquery);
 
 // get admin boolean
 
 // user matches db row
 if (mysqli_num_rows($result) == 1) {
   // set cookie before echo
-  setcookie('username', $username);
-
+  
   // TODO redirect to search page if not admin
-  // if ($username == 'mu' && $password == 'pw') {
-  //   header("Location: p.php");
-  //   exit();
-  // }
+  if ($username == 'mu' && $password == 'pw') {
+    setcookie('username', $username);
+    header("Location: productList.php");
+    exit();
+  }
+  setcookie('username', $username);
+  header("Location: productList.php");
+  exit();
+}
+else if (mysqli_num_rows($guestresult) == 1) {
+  // set cookie before echo
+  setcookie('username', 'guest');
   header("Location: productList.php");
   exit();
 }
