763547d02e885ad78aec34e502069f0c2568e283
tomjohnsonwi <92746305+tomjohnsonwi@users.noreply.github.com>
2023-02-27 10:13:36 -0600

    Update cart.php

--- project/final/cart.php
+++ project/final/cart.php
@@ -31,6 +31,11 @@
               <!-- Link -->
             <a class="list-anchor" href="logout.php">Logout</a>
           </li>
+          <li id="Back"
+              class="col nav-item">
+              <!-- Link -->
+            <a class="list-anchor" href="productList.php">Back to Product List</a>
+          </li>
         </ul>
       </div>
     </nav>
@@ -38,15 +43,21 @@
     <?php
       require_once('mysqli_connect.php');
 
+      // check connection
+      if (mysqli_connect_errno()) {
+        echo "Failed to connect to MySQL: " . mysqli_connect_error();
+        exit();
+      }
+
       // sanitize
-      $productName = $price = $category = $id = "";
-      $break = '<br>';
+      $productName = $price = $category = $id = $description = "";
 
       if ($_SERVER["REQUEST_METHOD"] == "POST") {
-        $productName = test_input($_POST["productName"]);
+        $productName = test_input($_POST["productname"]);
         $price = test_input($_POST["price"]);
         $category = test_input($_POST["category"]);
-        $id = test_input($_POST["id"]);
+        $id = test_input($_POST["prod_id"]);
+        $description = test_input($_POST["description"]);
       }
 
       function test_input($data) {
@@ -56,34 +67,74 @@
         return $data;
       }
 
-      // check connection
-      if (mysqli_connect_errno()) {
-        echo "Failed to connect to MySQL: " . mysqli_connect_error();
-        exit();
-      }
-      
-      // product added to cart from productList.php
-      // $query = "SELECT * FROM products WHERE ";
-    
-      // result
-      $result = mysqli_query($dbc, $query);
-      $row = mysqli_fetch_assoc($adminresult);
+      // Get the ID of the product to add to cart
+      if (isset($_POST['prod_id'])) {
+        $id = $_POST['prod_id'];
 
-      // results table
+        // Sanitize the ID value to prevent SQL injection
+        $id = mysqli_real_escape_string($dbc, $id);
+
+        $query = "INSERT INTO `cart`(
+          `username`, 
+          `productname`, 
+          `category`, 
+          `description`, 
+          `price`
+          ) VALUES (
+            '" . $_COOKIE['username'] . 
+            "', '" . $productName . 
+            "', '" . $category . 
+            "', '" . $description . 
+            "', '" . $price . 
+          "')";
+        $result = mysqli_query($dbc, $query);
+
+        // add to cart from productList.php
+        // $query = "insert into cart
+        //         (
+        //           username,
+        //           productname,
+        //           category, 
+        //           description, 
+        //           price
+        //         )
+        //         values (
+        //           ".$_COOKIE['username'].",
+        //           $productName,
+        //           $category,
+        //           $description,
+        //           $price
+        //         )
+        //         from users, product where id = $id";
+
+        // $result = mysqli_query($dbc, $query);
+
+        if ($result) {
+            // The product was successfully added to cart
+            echo "<div class='center'><h1>Product " . $id . " added.</h1></div>";
+        } else {
+            // There was an error while adding the product
+            echo "<div class='center'><h1>Error adding product.<br><br>Result: $result<br><br>$query</h1></div>";
+        }
+      } else {
+        // No ID was provided in the URL
+        echo "<div class='center'>Invalid request.</h1></div>";
+      }
+
+      // cart table
       echo "<table class='center' id='productNameTable'>";
-      echo "<th>Product Name</th><th>Price</th><th>Quantity</th><th>Category</th><th>Description</th>";
+      echo "<th>Product Name</th><th>Price</th><th>Category</th><th>Description</th>";
       while($row = mysqli_fetch_array($result, MYSQLI_ASSOC)) {
         echo "<tr><td>" 
         . $row["productname"] 
-        . "</td><td>" . $row["price"] 
-        . "</td><td>" . $row["quantity"] 
+        . "</td><td>" . $row["price"]
         . "</td><td>" . $row["category"] 
         . "</td><td>" . $row["description"]  
-        . "</tr>";
+        . "</td></tr>";
       }
       echo "</table>";
 
-      // close db
+      // Close the database connection
       mysqli_close($dbc);
     ?>
   </body>
